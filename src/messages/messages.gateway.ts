52247a7722fba3ca6af740c302ebb2ebcb890d0a
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-26 17:51:30 +0600

    inject MessagesService into gateway to reuse message business logic

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,7 +1,9 @@
 import { WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { MessagesService } from './messages.service';
 
 export class MessagesGateway {
   @WebSocketServer()
   server: Server;
+  constructor(private messagesService: MessagesService) {}
 }

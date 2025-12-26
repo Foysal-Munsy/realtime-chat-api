8aef21b18e0dadb6c33ebd67efe1a5728d4909d7
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-26 17:09:04 +0600

    added dotenv and fix database connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './database/database.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), MessagesModule],
+  imports: [TypeOrmModule.forRoot(databaseConfig), MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })

09cdfc71a8a7fcd42150aca61d389fb2e4267780
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-26 17:52:39 +0600

    [added] handle sendMessage event and broadcast new messages

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,9 +1,26 @@
-import { WebSocketServer } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { MessagesService } from './messages.service';
 
 export class MessagesGateway {
   @WebSocketServer()
   server: Server;
+
   constructor(private messagesService: MessagesService) {}
+
+  @SubscribeMessage('sendMessage')
+  async handleMessage(
+    @MessageBody() data: { content: string; sender: string },
+  ) {
+    const message = await this.messagesService.create(
+      data.content,
+      data.sender,
+    );
+    this.server.emit('newMessage', message);
+    return message;
+  }
 }

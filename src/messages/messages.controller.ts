ae11097be32b242af7e2c90cf817622b9daf3ead
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-26 17:22:54 +0600

    [added] add GET endpoint to retrieve all messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { Message } from './message.entity';
 
@@ -11,5 +11,10 @@ export class MessagesController {
     @Body() messageData: { content: string; sender: string },
   ): Promise<Message> {
     return this.messagesService.create(messageData.content, messageData.sender);
+  }
+
+  @Get()
+  findAll(): Promise<Message[]> {
+    return this.messagesService.findAll();
   }
 }

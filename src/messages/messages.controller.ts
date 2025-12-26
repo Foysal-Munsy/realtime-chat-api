e6752d6ccf2524fd1dc9b23451d483cb9f55790f
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-26 17:22:11 +0600

    [added] add POST endpoint to create and store messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { Message } from './message.entity';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private messagesService: MessagesService) {}
+
+  @Post()
+  create(
+    @Body() messageData: { content: string; sender: string },
+  ): Promise<Message> {
+    return this.messagesService.create(messageData.content, messageData.sender);
+  }
 }

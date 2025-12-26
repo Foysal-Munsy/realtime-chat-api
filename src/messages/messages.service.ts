bc6a3607a328bf55777be01c0b722037a8491d38
Foysal-Munsy <bdfoysal555@gmail.com>
2025-12-26 17:14:18 +0600

    [added] inject Message repository to enable db operations.

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Message } from './message.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectRepository(Message) private messageRepository: Repository<Message>,
+  ) {}
+}
